e69e972a9e2b51f4e553afc7d8b4e29abc184e81
ddlite92 <85956107+ddlite92@users.noreply.github.com>
2025-03-18 23:54:51 +0800

    Add files via upload

---
+++ blender/_batINTAfiq.bat
@@ -0,0 +1,5 @@
+C:\blender\blender -b R:\02_Production_2023\02_Eps\EP02-SabarPakAtan\DF7_EP02\lighting\blend_LGT.intvan\DF7_EP02_SH051.LGT.blend -a
+
+
+
+
---
+++ blender/_brenderEXT.sh
@@ -0,0 +1,9 @@
+blender -b /Documents/ScriptPY/Monsta/Blender/Path_Testing/BBBGS2_Gentar_Arc/2_Episode/Episode_16/6_Post/2_Work/1_Render/Part B/BBBGS2_EP16B_sh095/BBBGS2_EP16B_sh095_RENDER.blend --python ./test_console.py > file.txt
+blender -b /Documents/ScriptPY/Monsta/Blender/Path_Testing/BBBGS2_Gentar_Arc/2_Episode/Episode_16/6_Post/2_Work/1_Render/Part B/BBBGS2_EP16B_sh095/BBBGS2_EP16B_sh095_RENDER.blend 
+blender -b /Documents/ScriptPY/Monsta/Blender/Path_Testing/BBBGS2_Gentar_Arc/2_Episode/Episode_16/6_Post/2_Work/1_Render/Part B/BBBGS2_EP16B_sh095/BBBGS2_EP16B_sh095_RENDER.blend
+
+
+
+
+
+
